87d91ba0ae2a83cf42af50f95032ca355121cfc6
Jack-郑晓军 <jack-zheng@zamplus.com>
2019-02-16 17:34:37 +0800

    first commit

--- run.py
+++ run.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 from fe import app
 
-
 app.debug = True
 app.logger.debug('A value for debugging')
-app.run(host="0.0.0.0",port=5000,use_reloader=False)
+app.run(host="0.0.0.0", port=5000, use_reloader=False, threaded=True)
